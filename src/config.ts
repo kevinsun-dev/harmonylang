9deb9231830f74f92ffad311ebb73b0c2706bf4a
Anthony Yang <ay339@cornell.edu>
2021-02-13 18:11:53 -0500

    Add alternative method to cloud

--- src/config.ts
+++ src/config.ts
@@ -6,3 +6,6 @@ export const RESOURCE_DIR = path.join(EXTENSION_DIR, 'resource');
 export const CHARMONY_HTML_FILE = path.join(RESOURCE_DIR, "charmony-v2.html");
 
 export const HARMONY_SERVER_API = "https://harmonylang.herokuapp.com/";
+
+export const HARMONY_REGISTER_API = "http://localhost:8080/register";
+export const HARMONY_CHECK_API = (token: string) => `http://localhost:8080/check?token=${token}`;
--- src/feature/runServerAnalysis.ts
+++ src/feature/runServerAnalysis.ts
@@ -5,7 +5,50 @@ import * as FormData from 'form-data';
 import * as AdmZip from 'adm-zip';
 import * as tmp from 'tmp';
 import { IntermediateJson } from '../charmony/IntermediateJson';
-import { HARMONY_SERVER_API } from '../config';
+import {HARMONY_CHECK_API, HARMONY_REGISTER_API, HARMONY_SERVER_API} from '../config';
+
+async function doServerModelCheck (
+    projectDirectory: string,
+    mainFilename: string,
+    onFailModelCheck: (json: IntermediateJson) => void,
+    onOther: (msg: string) => void,
+    secondsAllotted: number = 60 * 10
+) {
+    const response = await axios.post(HARMONY_REGISTER_API, {mainFilename, secondsAllotted});
+    const token = response.data;
+
+    const bodyFormData = new FormData();
+    const zip = new AdmZip();
+    zip.addLocalFolder(projectDirectory, undefined, (filename) => {
+        return path.extname(filename).toLowerCase() == ".hny";
+    });
+    const tempFile = tmp.fileSync().name;
+    zip.writeZip(tempFile, async (err) => {
+        if (err != null) return;
+        bodyFormData.append("file", fs.createReadStream(tempFile));
+        try {
+            const response = await axios.post(HARMONY_CHECK_API(token),
+                bodyFormData,
+                {
+                    headers: {
+                        ...bodyFormData.getHeaders(),
+                    }
+                });
+            if (200 <= response.status && response.status < 300) {
+                const data = response.data;
+                if (data.status === "FAILURE") {
+                    const json: IntermediateJson = data.jsonData;
+                    return onFailModelCheck(json);
+                }
+                return onOther(data.message);
+            } else {
+                return onOther(response.data);
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    });
+}
 
 export function runServerAnalysis(
     projectDirectory: string,
